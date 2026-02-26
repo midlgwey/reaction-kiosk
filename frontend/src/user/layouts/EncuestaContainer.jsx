b84874b527e6be0720718da0117b104abd1dd884
David Sanchez Vargas <david.sanchezv201@tectijuana.edu.mx>
2026-02-26 12:58:30 -0800

    Cambio de color botones

--- frontend/src/user/layouts/EncuestaContainer.jsx
+++ frontend/src/user/layouts/EncuestaContainer.jsx
@@ -4,7 +4,6 @@ export default function EncuestaContainer() {
   return (
     <div className="
       min-h-screen 
-      bg-linear-to-br from-slate-50 to-gray-100
       flex items-center justify-center
       overflow-hidden
       p-4 md:p-10
