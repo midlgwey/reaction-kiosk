a40e96ae033a24837816d6432eba6f1ba86309da
David Sanchez Vargas <david.sanchezv201@tectijuana.edu.mx>
2026-02-26 13:03:03 -0800

    Cambio de color fondo

--- frontend/src/user/layouts/EncuestaContainer.jsx
+++ frontend/src/user/layouts/EncuestaContainer.jsx
@@ -2,15 +2,10 @@ import { Outlet } from "react-router-dom";
 
 export default function EncuestaContainer() {
   return (
-    <div className="
-      min-h-screen 
-      flex items-center justify-center
-      overflow-hidden
-      p-4 md:p-10
-    ">
-      <div className="w-full max-w-7xl">
-        <Outlet />
-      </div>
-    </div>
+   <div className="min-h-screen flex items-center justify-center p-4 md:p-10">
+
+    <Outlet />
+
+  </div>
   );
 }
