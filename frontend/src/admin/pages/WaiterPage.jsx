ac3642c1fa73e64172f2248512b4714d1d99847f
David Sanchez Vargas <david.sanchezv201@tectijuana.edu.mx>
2026-03-31 10:56:34 -0700

    Cambio en waiterpage

--- frontend/src/admin/pages/WaiterPage.jsx
+++ frontend/src/admin/pages/WaiterPage.jsx
@@ -7,20 +7,18 @@ const WaiterPage = () => {
   return (
     <div className="space-y-6">
 
-       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-          <div className="lg:col-span-8">
-            <WaiterPerformance />
-          </div>
-
-          <div className="lg:col-span-4">
-          
-          <LatestSuggestionsWidget />
-
-          </div>
-
-          <WaiterRanking />
-
-      </div>
+        <div className="bg-white border border-slate-200 p-6 rounded-3xl shadow-sm">
+              <WaiterPerformance />
+            </div>
+       
+            <div className="grid grid-cols-12 gap-6">
+              <div className="col-span-12 lg:col-span-4">
+                <WaiterRanking />
+              </div>
+              <div className="col-span-12 lg:col-span-8">
+                <LatestSuggestionsWidget />
+              </div>
+            </div>
           
     </div>
   )
