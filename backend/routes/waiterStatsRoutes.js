cd48df6457ad62cbdb128f0654765d1089e871c0
David Sanchez Vargas <david.sanchezv201@tectijuana.edu.mx>
2026-03-31 19:11:41 -0700

    fix: agregar ruta get-waitertables

--- backend/routes/waiterStatsRoutes.js
+++ backend/routes/waiterStatsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getWaitersTableRanking, getWaiterRadiography, getAllWaiters } from '../controllers/waiterStatsController.js';
+import { getWaitersTableRanking, getWaiterRadiography, getAllWaiters, getWaiterTables } from '../controllers/waiterStatsController.js';
 import { authenticateAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get('/get-waitersranking', authenticateAdmin, getWaitersTableRanking);
 router.get('/get-waitersradiography', authenticateAdmin, getWaiterRadiography);
 router.get('/get-allwaiters', authenticateAdmin, getAllWaiters); 
+router.get('/get-waitertables', authenticateAdmin, getWaiterTables);
 
 export default router;
