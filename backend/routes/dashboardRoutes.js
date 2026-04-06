3f8aa4914dbe28f4a4a9170af9c3ac61eadd1fcc
David Sanchez Vargas <david.sanchezv201@tectijuana.edu.mx>
2026-04-05 23:34:43 -0700

    fix: arreglo de ruta en el hook: daily-low-interaction

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -10,7 +10,7 @@ router.get('/daily-reactions', authenticateAdmin, getDailyReactions )
 //Ruta para la card de rating del servicio del dia
 router.get('/daily-serverscore', authenticateAdmin, getDailyServerScore)
 //Ruta para la card de indice de felicidad
-router.get('/daily-low-interaction ', authenticateAdmin, getLowInteractionWaiters)
+router.get('/daily-low-interaction', authenticateAdmin, getLowInteractionWaiters)
 //Ruta para la card de encuestas realizadas o no por dia
 router.get('/daily-survey-count', authenticateAdmin, getDailySurveyCount);
 //Ruta para la grafica de area de satisfaccion por dia
